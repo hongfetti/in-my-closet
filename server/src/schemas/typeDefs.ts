402e2ad67bb87e2b498e11c5edd007b7f6d55221
hongfetti <ryan.hong1994@gmail.com>
2025-03-17 10:50:11 -0400

    models need to be updated to match the rest of inputs - pulling develop to get the clothing item inputs

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -2,16 +2,63 @@ import gql from 'graphql-tag';
 
 const typeDefs = gql`
   type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    outfits: [Outfit]!
+    clothingItem: [ClothingItem]!
+  }
+
+  type ClothingItem {
+    _id: ID!
+    imageUrl: String!
+    clothingType: ClothingType!
+    color: String!
+    size: String!
+    season: Season!
+    createdAt: String!
+  }
+
+  enum ClothingType {
+    TOP
+    BOTTOM
+    OUTERWEAR
+  }
+
+  enum Season {
+    SPRING
+    SUMMER
+    FALL
+    WINTER
+  }
+
+  type Outfit {
+    _id: ID!
+    user: User!
+    top: ClothingItem!
+    bottom: ClothingItem!
+    outerwear: ClothingItem
   }
 
   input UserInput {
     username: String!
     email: String!
     password: String!
+  }
+
+  input ClothingItemInput {
+    imageUrl: String!
+    clothingType: ClothingType!
+    color: String!
+    size: String!
+    season: Season!
+  }
+
+  input OutfitInput {
+    topId: ID!
+    bottomId: ID!
+    outerwearId: ID!
   }
   
   type Auth {
@@ -23,11 +70,17 @@ const typeDefs = gql`
     users: [User]
     user(username: String!): User
     me: User
+    outfits: [Outfit]
+    outfit(id: ID!): Outfit
+    clothingItems: [ClothingItem]
+    clothingItem(id: ID!): ClothingItem
   }
 
   type Mutation {
     addUser(input: UserInput!): Auth
     login(email: String!, password: String!): Auth
+    addClothingItem(input: ClothingItemInput!): ClothingItem
+    addOutfit(input: OutfitInput!): Outfit
   }
 `;
 
