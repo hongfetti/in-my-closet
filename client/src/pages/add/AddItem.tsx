6f660a1ec41e5ee5a210f2a02ffe0a6b79961552
codeherstyle <jersey1993@icloud.com>
2025-03-17 16:14:19 -0400

    styling

--- client/src/pages/add/AddItem.tsx
+++ client/src/pages/add/AddItem.tsx
@@ -35,81 +35,8 @@ const Add = () => {
     
 
 
-        {/* Color Dropdown */}
-        <div className="dropdown btn-group">
-          <button
-            className="btn btn-secondary btn-lg dropdown-toggle"
-            type="button"
-            onClick={() => toggleDropdown("color")}
-            aria-expanded={openDropdown === "color"}
-          >
-            {selectedItems.color}
-          </button>
-          {openDropdown === "color" && (
-            <ul className="dropdown-menu show">
-              <li
-                className="dropdown-item"
-                onClick={() => handleSelect("color", "Red")}
-              >
-                Red
-              </li>
-              <li
-                className="dropdown-item"
-                onClick={() => handleSelect("color", "Green")}
-              >
-                Green
-              </li>
-              <li
-                className="dropdown-item"
-                onClick={() => handleSelect("color", "Blue")}
-              >
-                Blue
-              </li>
-              <li
-                className="dropdown-item"
-                onClick={() => handleSelect("color", "Yellow")}
-              >
-                Yellow
-              </li>
-              <li
-                className="dropdown-item"
-                onClick={() => handleSelect("color", "Orange")}
-              >
-                Orange
-              </li>
-              <li
-                className="dropdown-item"
-                onClick={() => handleSelect("color", "Pink")}
-              >
-                Pink
-              </li>
-              <li
-                className="dropdown-item"
-                onClick={() => handleSelect("color", "Black")}
-              >
-                Black
-              </li>
-              <li
-                className="dropdown-item"
-                onClick={() => handleSelect("color", "White")}
-              >
-                White
-              </li>
-              <li
-                className="dropdown-item"
-                onClick={() => handleSelect("color", "Grey")}
-              >
-                Grey
-              </li>
-              <li
-                className="dropdown-item"
-                onClick={() => handleSelect("color", "Multi-Color")}
-              >
-                Multi-Color
-              </li>
-            </ul>
-          )}
-        </div>
+    <main className="d-flex flex-column justify-content-center align-items-center min-vh-100">
+  <h1 className="add-item-title">ADD ITEM</h1>
 
   <form className="form d-flex flex-column align-items-center w-100">
     {/* Article Type Dropdown */}
@@ -134,6 +61,72 @@ const Add = () => {
       )}
     </div>
 
+  
+    <div className="dropdown btn-group w-50 text-center mt-3">
+      <button
+        className="btn dropdown-toggle w-100 py-2"
+        type="button"
+        onClick={() => toggleDropdown("size")}
+        aria-expanded={openDropdown === "size"}
+      >
+        {selectedItems.size}
+      </button>
+      {openDropdown === "size" && (
+        <ul className="dropdown-menu show w-100 text-center">
+          <li className="dropdown-item" onClick={() => handleSelect("size", "XS")}>XS</li>
+          <li className="dropdown-item" onClick={() => handleSelect("size", "S")}>S</li>
+          <li className="dropdown-item" onClick={() => handleSelect("size", "M")}>M</li>
+          <li className="dropdown-item" onClick={() => handleSelect("size", "L")}>L</li>
+          <li className="dropdown-item" onClick={() => handleSelect("size", "XL")}>XL</li>
+        </ul>
+      )}
+    </div>
+
+    <div className="dropdown btn-group w-50 text-center mt-3">
+      <button
+        className="btn dropdown-toggle w-100 py-2"
+        type="button"
+        onClick={() => toggleDropdown("color")}
+        aria-expanded={openDropdown === "color"}
+      >
+        {selectedItems.color}
+      </button>
+      {openDropdown === "color" && (
+        <ul className="dropdown-menu show w-100 text-center">
+          <li className="dropdown-item" onClick={() => handleSelect("color", "RED")}>RED</li>
+          <li className="dropdown-item" onClick={() => handleSelect("color", "BLUE")}>BLUE</li>
+          <li className="dropdown-item" onClick={() => handleSelect("color", "GREEN")}>GREEN</li>
+          <li className="dropdown-item" onClick={() => handleSelect("color", "YELLOW")}>YELLOW</li>
+          <li className="dropdown-item" onClick={() => handleSelect("color", "ORANGE")}>ORANGE</li>
+          <li className="dropdown-item" onClick={() => handleSelect("color", "PINK")}>PINK</li>
+          <li className="dropdown-item" onClick={() => handleSelect("color", "BLACK")}>BLACK</li>
+          <li className="dropdown-item" onClick={() => handleSelect("color", "WHITE")}>WHITE</li>
+          <li className="dropdown-item" onClick={() => handleSelect("color", "GREY")}>GREY</li>        
+          <li className="dropdown-item" onClick={() => handleSelect("color", "MULTI-COLOR")}>MULTI-COLOR</li>
+        </ul>
+      )}
+    </div>
+
+    <div className="dropdown btn-group w-50 text-center mt-3">
+      <button
+        className="btn dropdown-toggle w-100 py-2"
+        type="button"
+        onClick={() => toggleDropdown("season")}
+        aria-expanded={openDropdown === "season"}
+      >
+        {selectedItems.season}
+      </button>
+      {openDropdown === "season" && (
+        <ul className="dropdown-menu show w-100 text-center">
+          <li className="dropdown-item" onClick={() => handleSelect("season", "WINTER")}>WINTER</li>
+          <li className="dropdown-item" onClick={() => handleSelect("season", "SPRING")}>SPRING</li>
+          <li className="dropdown-item" onClick={() => handleSelect("season", "SUMMER")}>SUMMER</li>
+          <li className="dropdown-item" onClick={() => handleSelect("season", "FALL")}>FALL</li>
+        </ul>
+      )}
+    </div>
+
+
         <div className="upload">
           <UploadWidget setImageUrl={handleImageUpload} />
           {selectedItems.image_url && (
@@ -145,12 +138,13 @@ const Add = () => {
           )}
         </div>
 
-        <button type="button" className="btn btn-primary">
-          Add to closet
-        </button>
-      </form>
-    </main>
+        <button type="button" className="btn add-button mt-4 w-50 text-center">
+      Add to Closet
+    </button>
+  </form>
+</main>
   );
 };
+
 
 export default Add;
