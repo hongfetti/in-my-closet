ee252fbd40e6c59aca792f575c582a3e17868051
hongfetti <ryan.hong1994@gmail.com>
2025-03-18 12:08:51 -0400

    fixed outfit.ts

--- server/src/models/Outfit.ts
+++ server/src/models/Outfit.ts
@@ -6,7 +6,10 @@ interface IOutfit extends Document {
     userId: Schema.Types.ObjectId;
     topId: Schema.Types.ObjectId;
     bottomId: Schema.Types.ObjectId;
+    dressJumpsuitId?: Schema.Types.ObjectId;
+    shoesId?: Schema.Types.ObjectId;
     outerwearId?: Schema.Types.ObjectId;
+    accessoriesId?: Schema.Types.ObjectId;
 }
 
 const outfitSchema = new Schema<IOutfit>(
@@ -25,7 +28,7 @@ const outfitSchema = new Schema<IOutfit>(
             validate: {
                 validator: async function (value: Types.ObjectId) {
                     const item = await ClothingItem.findById(value);
-                    return item?.articleType === "Top";
+                    return item?.articleType === "TOP";
                 },
                 message: "topId must refer to an item of the type 'Top'"
             }
@@ -37,9 +40,33 @@ const outfitSchema = new Schema<IOutfit>(
             validate: {
                 validator: async function (value: Types.ObjectId) {
                     const item = await ClothingItem.findById(value);
-                    return item?.articleType === "Bottom";
+                    return item?.articleType === "BOTTOM";
                 },
                 message: "bottomId must refer to an item of the type 'Bottom'"
+            }
+        },
+        dressJumpsuitId: {
+            type: Schema.Types.ObjectId,
+            ref: "ClothingItems",
+            required: true,
+            validate: {
+                validator: async function (value: Types.ObjectId) {
+                    const item = await ClothingItem.findById(value);
+                    return item?.articleType === "DRESS_JUMPSUIT";
+                },
+                message: "dressJumpsuitId must refer to an item of the type 'Dress/Jumpsuit'"
+            }
+        },
+        shoesId: {
+            type: Schema.Types.ObjectId,
+            ref: "ClothingItems",
+            required: true,
+            validate: {
+                validator: async function (value: Types.ObjectId) {
+                    const item = await ClothingItem.findById(value);
+                    return item?.articleType === "SHOES";
+                },
+                message: "shoesId must refer to an item of the type 'Shoes'"
             }
         },
         outerwearId: {
@@ -49,9 +76,21 @@ const outfitSchema = new Schema<IOutfit>(
             validate: {
                 validator: async function (value: Types.ObjectId) {
                     const item = await ClothingItem.findById(value);
-                    return item?.articleType === "Outerwear";
+                    return item?.articleType === "OUTERWEAR";
                 },
                 message: "outerwearId must refer to an item of the type 'Outerwear'"
+            }
+        },
+        accessoriesId: {
+            type: Schema.Types.ObjectId,
+            ref: "ClothingItems",
+            required: true,
+            validate: {
+                validator: async function (value: Types.ObjectId) {
+                    const item = await ClothingItem.findById(value);
+                    return item?.articleType === "ACCESSORIES";
+                },
+                message: "accessoriesId must refer to an item of the type 'Accessories'"
             }
         },
     }
