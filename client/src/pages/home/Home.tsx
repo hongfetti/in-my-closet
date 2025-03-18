79c263e610c88840eaa40ef15cc6030cfd8367e2
codeherstyle <jersey1993@icloud.com>
2025-03-18 15:43:14 -0400

    update

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -3,15 +3,15 @@ import Carousel from "../../components/carousel/Carousel";
 
 const Home = () => {
   return (
-    <main>
+    <main className="text-center" style={{ marginTop: '80px' }}>
       <section className="container">
-        <h1>Style It Up</h1>
+        <h1 className="text-primary" style={{ backgroundColor: '#7669EA', color: 'white', padding: '10px', borderRadius: '10px' }}>Style It Up</h1>
 
         <Carousel id="carouselTop" />
         <Carousel id="carouselBottom" />
       </section>
       <Link className="page-nav" to="/add">
-        <button className="btn-add">Add Items</button>
+        <button className="btn" style={{ backgroundColor: '#7669EA', color: 'white', marginTop: '20px', padding: '10px 20px', borderRadius: '10px', width: '150px' }}>Add Items</button>
       </Link>
     </main>
   );
