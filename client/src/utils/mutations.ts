7e4bb5c6ff08d5a43fdb7c3377118180f889f7c7
codeherstyle <jersey1993@icloud.com>
2025-03-19 16:38:13 -0400

    added delete but to each clothing item in wardrobe

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -78,6 +78,12 @@ export const DELETE_CLOTHING_ITEM = gql`
   mutation deleteClothingItem($input: DeleteClothingItemInput!) {
     deleteClothingItem(input: $input) {
       _id
+    image_url
+    articleType
+    color
+    size
+    season
+    createdAt
     }
   }
 `;
