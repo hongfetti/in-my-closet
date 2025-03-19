a4585e3ed06a9393f121f98cf86601836744b433
dlastname <dllorens28@gmail.com>
2025-03-19 12:00:05 -0400

    more home logged in logic

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Carousel from "../../components/carousel/Carousel";
 import { useQuery } from "@apollo/client";
 import { GET_CURRENT_USER } from "../../utils/queries";
+import auth from "../../utils/auth";
 
 const Home = () => {
   const { data } = useQuery(GET_CURRENT_USER);
@@ -23,17 +24,89 @@ const Home = () => {
     ) ?? [];
 
   return (
-    <main className="text-center" style={{ marginTop: '80px' }}>
-      <section className="container">
-        <h1 className="text-primary" style={{ backgroundColor: '#7669EA', color: 'white', padding: '10px', borderRadius: '10px' }}>Style It Up</h1>
+    <div>
+      {auth.loggedIn() ? (
+        <main className="text-center" style={{ marginTop: "80px" }}>
+          <section className="container">
+            <h1
+              className="text-primary"
+              style={{
+                backgroundColor: "#7669EA",
+                color: "white",
+                padding: "10px",
+                borderRadius: "10px",
+              }}
+            >
+              Style It Up
+            </h1>
 
-        <Carousel id="carouselTop" images={userTopImageURLS} />
-        <Carousel id="carouselBottom" images={userBottomImageURLS} />
-      </section>
-      <Link className="page-nav" to="/add">
-        <button className="btn" style={{ backgroundColor: '#7669EA', color: 'white', marginTop: '20px', padding: '10px 20px', borderRadius: '10px', width: '150px' }}>Add Items</button>
-      </Link>
-    </main>
+            <Carousel id="carouselTop" images={userTopImageURLS} />
+            <Carousel id="carouselBottom" images={userBottomImageURLS} />
+          </section>
+          <Link className="page-nav" to="/add">
+            <button
+              className="btn"
+              style={{
+                backgroundColor: "#7669EA",
+                color: "white",
+                marginTop: "20px",
+                padding: "10px 20px",
+                borderRadius: "10px",
+                width: "150px",
+              }}
+            >
+              Add Items
+            </button>
+          </Link>
+        </main>
+      ) : (
+        <main className="text-center" style={{ marginTop: "80px" }}>
+          <section className="container">
+            <h1
+              className="text-primary mb-4" 
+              style={{
+                backgroundColor: "#7669EA",
+                color: "white",
+                padding: "10px",
+                borderRadius: "10px",
+              }}
+            >
+              Style It Up
+            </h1>
+
+
+            <div className="d-flex justify-content-center gap-3">
+              <Link
+                to="/login"
+                className="text-primary fs-4" 
+                style={{
+                  backgroundColor: "#7669EA",
+                  color: "white",
+                  padding: "10px",
+                  borderRadius: "10px",
+                  textDecoration: "none",
+                }}
+              >
+                Login
+              </Link>
+              <Link
+                to="/signup"
+                className="text-primary fs-4" 
+                style={{
+                  backgroundColor: "#7669EA",
+                  color: "white",
+                  padding: "10px",
+                  borderRadius: "10px",
+                  textDecoration: "none",
+                }}
+              >
+                Sign Up
+              </Link>
+            </div>
+          </section>
+        </main>
+      )}
+    </div>
   );
 };
 
